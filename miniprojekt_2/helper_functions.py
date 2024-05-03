da168fa129f1d7100e1cdcdd3241bb3a8241f10f
Bruno Sienkiewicz <bruno.sienkiewicz@gmail.com>
2024-05-03 10:52:17 +0200

    git fix

---
+++ miniprojekt_2/helper_functions.py
@@ -0,0 +1,221 @@
+import torch
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+
+from tqdm import tqdm
+
+
+def train_model(model, train, valid, criterion, optimizer, eval_func, batch_size=100, num_epochs=1000, verbose=True, epoch_interval=100, device=torch.device('cuda')):
+    train_loader = torch.utils.data.DataLoader(train, batch_size=batch_size, shuffle=True)
+
+    iters_list = []
+    loss_list = []
+    train_eval_list = []
+    valid_eval_list = []
+
+    _range = range(num_epochs)
+    if verbose:
+        _range = tqdm(_range)
+    for epoch in _range:
+        for i, (x, y) in enumerate(train_loader):
+            x = x.to(device)
+            y = y.to(device)
+            model.train()
+            y_pred = model(x)
+            y_pred = y_pred.squeeze()
+            loss = criterion(y_pred, y)
+            loss.backward()
+            optimizer.step()
+            optimizer.zero_grad()
+
+        if epoch % epoch_interval == 0:
+            iters_list.append(epoch)
+            loss_list.append(loss.item())
+            train_eval_list.append(eval_func(model, train))
+            valid_eval_list.append(eval_func(model, valid))
+
+    return iters_list, loss_list, train_eval_list, valid_eval_list
+
+
+def train_model_w(model, train, valid, criterion, optimizer, eval_func, batch_size=100, num_epochs=1000, verbose=True, epoch_interval=100, device=torch.device('cuda')):
+    train_loader = torch.utils.data.DataLoader(train, batch_size=batch_size, shuffle=True)
+
+    iters_list = []
+    loss_list = []
+    train_eval_list = []
+    valid_eval_list = []
+
+    _range = range(num_epochs)
+    if verbose:
+        _range = tqdm(_range)
+    for epoch in _range:
+        for i, (x, y) in enumerate(train_loader):
+            y = y.type(torch.LongTensor)
+            x = x.to(device)
+            y = y.to(device)
+            model.train()
+            y_pred = model(x)
+            y_pred = y_pred.squeeze()
+            loss = criterion(y_pred, y)
+            loss.backward()
+            optimizer.step()
+            optimizer.zero_grad()
+
+        if epoch % epoch_interval == 0:
+            iters_list.append(epoch)
+            loss_list.append(loss.item())
+            train_eval_list.append(eval_func(model, train))
+            valid_eval_list.append(eval_func(model, valid))
+
+    return iters_list, loss_list, train_eval_list, valid_eval_list
+
+
+def plot_training(iters, loss, train_eval, valid_eval, ax=None):
+
+    if ax is None:
+        fig, ax1 = plt.subplots()
+    else:
+        ax1 = ax
+
+    color = 'tab:red'
+    ax1.set_xlabel('Epoch')
+    ax1.set_ylabel('Loss', color=color)
+    ax1.plot(iters, loss, color=color)
+    ax1.tick_params(axis='y', labelcolor=color)
+
+    ax2 = ax1.twinx()
+    color = 'tab:blue'
+    ax2.set_ylabel('Evaluation', color=color)
+    ax2.plot(iters, train_eval, color=color, linestyle='dashed', label='Train')
+    ax2.plot(iters, valid_eval, color=color, linestyle='solid', label='Validation')
+    ax2.tick_params(axis='y', labelcolor=color)
+
+    if ax is None:
+        fig.tight_layout()
+        plt.legend()
+        plt.show()
+
+
+def get_confusion_matrix(model, data, device=torch.device('cuda')):
+    model.eval()
+    final_score=0
+    for x, y in torch.utils.data.DataLoader(data, batch_size=len(data)):
+        x = x.to(device)
+        y = y.to(device)
+        y_pred = model(x)
+        y_pred = torch.round(y_pred)
+        y = y.view_as(y_pred)
+        confusion_matrix = torch.zeros(3, 3)
+        for i in range(3):
+            present_score=0
+            for j in range(3):
+                confusion_matrix[i, j] = torch.sum((y == i) & (y_pred == j))
+                present_score+=confusion_matrix[i,j]
+            final_score+=min(1,confusion_matrix[i,i]/present_score)
+    model.train()
+    fig, ax = plt.subplots(1, 1, figsize=(10, 10))
+    sns.heatmap(confusion_matrix, annot=True, ax=ax, fmt='g')
+    ax.set_xlabel("Predicted")
+    ax.set_ylabel("True")
+    fig.suptitle("Confusion matrix")
+    fig.tight_layout()
+    plt.show()
+    print(final_score/3)
+
+
+def get_confusion_matrix_w(model, data):
+    model.eval()
+    with torch.no_grad():
+        for x, y in torch.utils.data.DataLoader(data, batch_size=len(data)):
+            x, y = x.cuda(), y.cuda()
+            outputs = model(x)
+            _, predicted = torch.max(outputs.data, 1)
+            confusion = pd.crosstab(y.cpu().numpy(), predicted.cpu().numpy(), rownames=['Actual'], colnames=['Predicted'])
+            sns.heatmap(confusion, annot=True, fmt='d')
+            plt.show()
+    model.train()
+
+
+def get_balanced_accuracy(model, data, device=torch.device('cuda')):
+    model.eval()
+    with torch.no_grad():
+        for x, y in torch.utils.data.DataLoader(data, batch_size=len(data)):
+            x, y = x.to(device), y.to(device)
+            outputs = model(x)
+            _, predicted = torch.max(outputs.data, 1)
+            confusion = pd.crosstab(y.cpu().numpy(), predicted.cpu().numpy(), rownames=['Actual'], colnames=['Predicted'])
+            balanced_accuracy = 0
+            for i in range(confusion.shape[0]):
+                balanced_accuracy += confusion.iloc[i, i] / confusion.iloc[i, :].sum()
+            balanced_accuracy /= confusion.shape[0]
+    model.train()
+    return balanced_accuracy
+
+
+def accuracy_weighted(model, data):
+    correct = 0
+    total = 0
+    model.eval()
+    with torch.no_grad():
+        for x, y in torch.utils.data.DataLoader(data, batch_size=len(data)):
+            x, y = x.cuda(), y.cuda()
+            outputs = model(x)
+            _, predicted = torch.max(outputs.data, 1)
+            total += y.size(0)
+            correct += (predicted == y).sum().item()
+    model.train()
+    return correct / total
+
+def get_mse(model, data, device=torch.device('cuda')):
+    model.eval()
+    for x, y in torch.utils.data.DataLoader(data, batch_size=len(data)):
+        x = x.to(device)
+        y = y.to(device)
+        y_pred = model(x)
+        mse = torch.mean((y_pred - y) ** 2)
+    model.train()
+    return mse.item()
+
+def get_accuracy(model, data, device=torch.device('cuda')):
+    correct = 0
+    total = 0
+    model.eval()
+    for x, y in torch.utils.data.DataLoader(data, batch_size=len(data)):
+        x = x.to(device)
+        y = y.to(device)
+        output = model(x)
+        y_pred = torch.round(output)
+        correct = y_pred.eq(y.view_as(y_pred)).sum().item()
+        total = y_pred.shape[0]
+    model.train()
+    if ((correct / total)>0.20):
+      return max(correct / total)
+    else:
+      return get_accuracy_regression(model,data)
+
+
+def get_accuracy_regression(model, data, device=torch.device('cuda')):
+    correct = 0
+    total = 0
+    model.eval()
+    for x, y in torch.utils.data.DataLoader(data, batch_size=len(data)):
+        x = x.to(device)
+        y = y.to(device)
+        output = model(x)
+        for prediction, true_value in zip(output, y):
+            if true_value <=100000 and 100000 >= prediction :
+                correct += 1
+            elif true_value>100000 and true_value<350000 and 100000 <= prediction and prediction<350000:
+                correct += 1
+            elif true_value>=350000 and 350000 >= prediction :
+                correct += 1
+        total += y.shape[0]
+    return correct / total
+
+
+
+def get_overfitting(model, train_data, valid_data, eval_func):
+    train_eval = eval_func(model, train_data)
+    valid_eval = eval_func(model, valid_data)
+    return train_eval - valid_eval
---
+++ miniprojekt_3/helper_functions.py
@@ -0,0 +1,72 @@
+import torch
+import torchvision
+import torch.nn as nn
+import torchvision.transforms as transforms
+import pandas as pd
+import numpy as np
+import torch.nn.functional as F
+import matplotlib.pyplot as plt
+
+from tqdm import tqdm
+
+
+class FastDataset(torch.utils.data.Dataset):
+    def __init__(self, data, labels):
+        self.data = data
+        self.labels = labels
+        
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        return self.data[idx], self.labels[idx]
+    
+
+def train_model(model, train_loader, val_loader, criterion, optimizer, eval_fn, device, num_epochs=5, verbose=True):
+    train_eval_hist = []
+    val_eval_hist = []
+    loss_hist = []
+    if verbose:
+        rng = tqdm(range(num_epochs))
+    else:
+        rng = range(num_epochs)
+    
+    for _ in rng:
+        model.train()
+        for images, labels in train_loader:
+            images, labels = images.to(device), labels.to(device)
+            optimizer.zero_grad()
+            outputs = model(images)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+        loss_hist.append(loss.item())
+        train_eval_hist.append(eval_fn(model, train_loader, device))
+        val_eval_hist.append(eval_fn(model, val_loader, device))
+    return loss_hist, train_eval_hist, val_eval_hist
+
+def plot_training(iters, loss, train_eval, valid_eval, ax=None):
+
+    if ax is None:
+        fig, ax1 = plt.subplots()
+    else:
+        ax1 = ax
+
+    color = 'tab:red'
+    ax1.set_xlabel('Epoch')
+    ax1.set_ylabel('Loss', color=color)
+    ax1.plot(iters, loss, color=color)
+    ax1.tick_params(axis='y', labelcolor=color)
+
+    ax2 = ax1.twinx()
+    color = 'tab:blue'
+    ax2.set_ylabel('Evaluation', color=color)
+    ax2.plot(iters, train_eval, color=color, linestyle='dashed', label='Train')
+    ax2.plot(iters, valid_eval, color=color, linestyle='solid', label='Validation')
+    ax2.tick_params(axis='y', labelcolor=color)
+
+    if ax is None:
+        fig.tight_layout()
+        plt.legend()
+        plt.show()
+         
